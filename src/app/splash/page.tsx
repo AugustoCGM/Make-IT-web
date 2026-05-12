efe37de68fb9b3931440b5bd8c879cd53102695d
Augusto Moreno <augusto.cgm020306@gmail.com>
2026-05-12 19:13:30 -0300

    Atualização da tela de Splash

--- src/app/splash/page.tsx
+++ src/app/splash/page.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react"
 import { useRouter } from "next/navigation"
 import Image from "next/image"
 import { Container } from "./styles"
+import { BlurBlob } from "../components/BackgroundBlobs/styles";
 
 export default function Splash() { 
     const router = useRouter();
@@ -17,6 +18,7 @@ export default function Splash() {
   }, [router]);
 
   return (
+    <div style={{ backgroundColor: '#FEFEFE', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', position: 'relative', overflow: 'hidden', zIndex: 0 }}>
     <Container>
      <Image
     src="/Logo_02.png"
@@ -25,6 +27,18 @@ export default function Splash() {
     height={58}
 />
     </Container>
+
+<BlurBlob $color="#5184F0" $size="500px" $bottom="-30%" $right="-5%" />
+
+<BlurBlob $color="#FFF38A" $size="300px" $bottom="0%" $left="10%" />
+
+<BlurBlob $color="#5184F0" $size="350px" $top="30%" $left="-5%" />
+
+<BlurBlob $color="#F472D0" $size="400px" $bottom="-15%" $left="-10%" />  
+
+<BlurBlob $color="#F472D0" $size="300px" $bottom="5%" $right="-10%" />
+
+    </div>
   );
 }
 
