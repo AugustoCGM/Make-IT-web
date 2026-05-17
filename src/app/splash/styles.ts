8b8d5db3f2eb54a3bd06e08e5f634208fe02568e
Augusto Moreno <augusto.cgm020306@gmail.com>
2026-05-17 11:54:05 -0300

    atualização e correção da tela de splash

--- src/app/splash/styles.ts
+++ src/app/splash/styles.ts
@@ -6,4 +6,15 @@ export const Container = styled.div`
     align-items: center;
     height: 100vh;
     background-color: #FEFEFE;
+
+    <BlurBlob $color="#5184F0" $size="500px" $bottom="-30%" $right="-5%" />
+    
+    <BlurBlob $color="#FFF38A" $size="300px" $bottom="0%" $left="10%" />
+    
+    <BlurBlob $color="#5184F0" $size="350px" $top="30%" $left="-5%" />
+    
+    <BlurBlob $color="#F472D0" $size="400px" $bottom="-15%" $left="-10%" />  
+    
+    <BlurBlob $color="#F472D0" $size="300px" $bottom="5%" $right="-10%" />
+    
 `;
